bcdc60e2086102ae6ce1ef3500cfebf9538911bf
nsj3 <Stephen.Juggins@ncl.ac.uk>
2014-02-10 19:31:58 +0000

    Commit SJ version

---
+++ SimulatedData.R
@@ -0,0 +1,123 @@
+setwd("d:\\Data\\GitHub\\randomwa")
+# source("d:\\Data\\R_Libraries\\People\\Functions\\Functions.r")
+# source("Functions.r")
+library(vegan)
+library(rioja)
+library(compas)
+source("CorrelateXY.r")
+
+make.core <- function(n, x=50, y=20, effect=40) {
+   xd <- density(sqrt(rlnorm(10000)), n=nCore)
+   mx <- max(xd$y)
+   xdd.x <- round((xd$y / mx * effect) / 2) * 2 + y
+   xdd.y <- rep(x, times=length(xdd.x))
+   xdd <- cbind(GRAD1=xdd.y, GRAD2=xdd.x)
+   xdd
+}
+
+make.mix <- function(inc) {
+   sq <- expand.grid(inc, inc)
+   sq <- sq[rowSums(sq) <= max(inc), ]
+   sq <- cbind(sq, max(inc)-rowSums(sq))
+   sq <- sq / rowSums(sq)
+   colnames(sq) <- c("Mixed", "Grad1", "Grad2")
+   sq
+}
+
+sim <- function(nsam=200, nsp=c(50, 50, 50), core=make.core(100, 50, 20), corr=0.0, G1.range=c(0, 100), G2.range=c(0, 100)) {
+   nCore <- nrow(core)
+   rr <- mvtunif(nsam, corr = corr)
+   G2.r <- diff(G2.range)
+   if (G2.r < 100) {
+      r <- range(rr[, 2])
+      rr[, 2] <- (rr[, 2] - r[1]) / diff(r) * G2.r + G2.range[1]
+   }
+   G1.r <- diff(G1.range)
+   if (G1.r < 100) {
+      r <- range(rr[, 1])
+      rr[, 1] <- (rr[, 1] - r[1]) / diff(r) * G1.r + G1.range[1]
+   }
+   control1 <- compas.control(sam.type="user", user.samples=rr, ngrad=2, nsp=nsp[1], seed=-1, quantn=T, qualn=T)
+   control1$IPARAM["QUANTN"] <- 1
+   control1$IPARAM["IWPAR"] <- 3
+   control1$IPARAM["IFDNOI"] <- 3
+   control1$DPARAM[1] <- 50
+   control1$DPARAM[5] <- .1
+   control2 <- control1
+   control3 <- control1
+   sam1 <- compas.sam(control1)
+   control2$ngrad<- 1
+   control2$nsp=nsp[2]
+   control2$user.samples <- sam1$X[, 1, drop=FALSE]
+   control3$ngrad<- 1
+   control3$nsp=nsp[3]
+   control3$user.samples <- sam1$X[, 2, drop=FALSE]
+   sam2 <- compas.sam(control2)
+   sam3 <- compas.sam(control3)
+   sp1 <- compas.sp(control1)
+   sp2 <- compas.sp(control2)
+   sp3 <- compas.sp(control3)
+   comm1 <- compas(control1, spec=sp1, sam=sam1)
+   comm2 <- compas(control2, spec=sp2, sam=sam2)
+   comm3 <- compas(control3, spec=sp3, sam=sam3)
+   
+   controlf <- compas.control(sam.type="user", user.samples=core, ngrad=2, quantn=F, qualn=F)
+   fsam1 <- compas.sam(controlf)
+   foss1 <- compas(controlf, spec=sp1, sam=fsam1)
+   controlf$user.samples <- core[, 1, drop=FALSE]
+   controlf$ngrad <- 1
+   fsam2 <- compas.sam(controlf)
+   foss2 <- compas(controlf, spec=sp2, sam=fsam2)
+   controlf$user.samples <- core[, 2, drop=FALSE]
+   fsam3 <- compas.sam(controlf)
+   foss3 <- compas(controlf, spec=sp3, sam=fsam3)
+   
+   comm <- data.frame(comm1$A, comm2$A, comm3$A)
+   sel <- colSums(comm) > 0
+   comm <- comm[, sel]
+   foss <- data.frame(foss1$A, foss2$A, foss3$A)
+   foss <- foss[, sel]
+   sel <- apply(comm, 1, sum) > 0
+   rk <- rr
+   comm <- comm[sel, ]
+   rk <- rr[sel, ]
+   
+   list(spec=comm, env=rk, foss=foss, core=core)
+   
+}
+   
+plot.sim <- function(simul, nTaxa=30) {
+   par(mfrow=c(2,2))
+   rWA1 <- randomWA.SJ(simul$spec, simul$env[, 1])
+   rWA2 <- randomWA.SJ(simul$spec, simul$env[, 2])
+   VI1.names <- rownames(rWA1$VI)
+   VI2.names <- rownames(rWA2$VI)
+   plot(rWA1)
+   plot(rWA2)
+   wa1.all <- WA(simul$spec, simul$env[, 1])
+   wa2.all <- WA(simul$spec, simul$env[, 2])
+   y1 <- simul$spec[, VI1.names[1:nTaxa]]
+   y1 <- y1[, apply(y1, 2, sum) > 0]
+   sel <- apply(y1, 1, sum) > 0
+   wa1.sel <- WA(y1[sel, ], simul$env[sel, 1])
+   y2 <- simul$spec[, VI2.names[1:nTaxa]]
+   y2 <- y2[, apply(y2, 2, sum) > 0.1]
+   sel <- apply(y2, 1, sum) > 0
+   wa2.sel <- WA(y2[sel, ], simul$env[sel, 2])
+   foss1.all <- predict(wa1.all, simul$foss)$fit
+   foss2.all <- predict(wa2.all, simul$foss)$fit
+   foss1.sel <- predict(wa1.sel, simul$foss)$fit
+   foss2.sel <- predict(wa2.sel, simul$foss)$fit
+   plot(1:100, 1:100, type="n", ylim=c(10, 70))
+   lines(1:100, simul$core[, 1], col="red")
+   lines(1:100, foss1.all[, 1], col="red", lty=2)
+   lines(1:100, foss1.sel[, 1], col="red", lty=3)
+   plot(1:100, 1:100, type="n", ylim=c(10, 70))
+   lines(1:100, simul$core[, 2], col="blue")
+   lines(1:100, foss2.all[, 1], col="blue", lty=2)
+   lines(1:100, foss2.sel[, 1], col="blue", lty=3)
+}
+
+simul <- sim(nsp=c(50, 50, 50), corr=0.3)
+
+plot.sim(simul, nTaxa=20)
---
+++ randomWA_SJ.r
@@ -0,0 +1,107 @@
+library(rioja)
+data(SWAP)
+
+setwd("d:\\data\\GitHub\\randomwa")
+
+set.seed(1)
+
+randomWA.SJ <- function(x, ...) UseMethod("randomWA.SJ")
+
+randomWA.SJ.default <- function (spec, env, nVar, nTF=500, verbose=TRUE) {
+   y <- as.matrix(spec)
+   x <- as.matrix(env)
+   nsam <- nrow(y)
+   nsp <- ncol(spec)
+   if (missing(nVar))   
+      nVar <- max(as.integer(nsp/3), 1)
+   res <- matrix(ncol=nsp, nrow=nTF)
+   colnames(res) <- colnames(y)
+   if (verbose) {
+      writeLines("Fitting models:")
+      pb <- txtProgressBar(min=0, max=nTF, style=3)
+      on.exit(close(pb))
+   }
+   for (i in 1:nTF) {
+      if (verbose)
+          setTxtProgressBar(pb, i)
+      sel.sp <- sample.int(nsp, nVar)
+      # bootstrap and test set sample indices      
+      boot <- sample.int(nsam, replace=TRUE)
+      test <- setdiff(1:nsam, boot)
+      x.b <- x[boot]
+      y.b <- y[boot, sel.sp]
+      x.t <- x[test]
+      y.t <- y[test, sel.sp]
+      # remove taxa with no occur in bootstrap sample
+      sel <- apply(y.b, 2, sum) > 0
+      y.b <- y.b[, sel]
+      y.t <- y.t[, sel]
+      # remove samples with no taxa
+      sel <- apply(y.b, 1, sum) > 0
+      y.b <- y.b[sel, ]
+      x.b <- x.b[sel]
+      mod <- WA.fit(y.b, x.b, lean=TRUE)
+      pred.oob <- predict.internal.WA(mod, y.t, lean=TRUE)
+      mseOOB <- mean((pred.oob[, 1] - x.t)^2)
+      nv <- ncol(y.t)
+      cnms <- colnames(y.t)
+      nsam.t <- nrow(y.t)
+      for (j in 1:nv) {
+         y.t2 <- y.t
+         y.t2[, j] <- y.t[sample.int(nsam.t), j]
+         pred.oob_h <- predict.internal.WA(mod, y.t2, lean=TRUE)
+         mseOOB_H <- mean((pred.oob_h[, 1] - x.t)^2)
+         res[i, cnms[j]] <- mseOOB_H - mseOOB
+      }
+   }
+   SumErr <- colSums(res, na.rm=TRUE)
+   nTree <- colSums(!is.na(res)) 
+   VI <- SumErr / nTree
+   names(VI) <- colnames(y)
+   VI <- data.frame(VI=sort(VI, decreasing=TRUE))
+   res <- list(res=res, VI=VI, spec=y, env=x)
+   class(res) <- "randomWA.SJ"
+   res
+}
+
+
+x <- rWA
+
+plot.randomWA.SJ <- function(x) {
+   nSams <- seq(10, nrow(x$VI), by=10)
+   if (max(nSams) < nrow(x$VI)) {
+      nSams[length(nSams)+1] <- nrow(x$VI)
+   }
+   res <- numeric(length(nSams))
+   cnames <- rownames(x$VI)
+   for (i in 1:length(nSams)) {
+      y1 <- x$spec[, cnames[1:nSams[i]]]
+      sel <- rowSums(y1) > 0.0
+      mod <- WA(y1[sel, ], x$env[sel])
+      res[i] <- rioja:::performance(mod)$object[1, 1]
+   }
+   plot(nSams, res, type="b", xlab="nTaxa", ylab="RMSE")
+}
+
+
+if (0) {
+
+VI <- randomWA.SJ(SWAP$spec, SWAP$pH, nTF=1000)
+
+y <- SWAP$spec
+x <- SWAP$pH
+
+sam <- seq(10, ncol(y), by=10)
+cnames <- rownames(VI$VI)
+
+
+for (i in 1:length(sam)) {
+   y1 <- y[, cnames[1:sam[i]]]
+   mod <- WA(y1, x)
+   res[i] <- rioja:::performance(mod)$object[1, 1]
+}
+
+
+
+
+}
